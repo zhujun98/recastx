4d667977843b7586e5059f232809c886f555937b
Jun Zhu <zhujun981661@gmail.com>
2022-06-01 11:12:21 +0200

    Add unitest for computing reciprocal

--- slicerecon/include/slicerecon/reconstruction/utils.hpp
+++ slicerecon/include/slicerecon/reconstruction/utils.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include <Eigen/Eigen>
+#include <spdlog/spdlog.h>
 
 #ifndef ASTRA_CUDA
 #define ASTRA_CUDA
@@ -13,6 +14,8 @@
 #include "astra/ParallelProjectionGeometry3D.h"
 #include "astra/ParallelVecProjectionGeometry3D.h"
 #include "astra/VolumeGeometry3D.h"
+
+#include "../data_types.hpp"
 
 namespace slicerecon::utils {
 
@@ -34,5 +37,50 @@ slice_transform(Eigen::Vector3f base, Eigen::Vector3f axis_1,
 
 std::string info(const astra::CConeVecProjectionGeometry3D& x);
 std::string info(const astra::CVolumeGeometry3D& x);
+
+template<typename T>
+inline void averageImages(const std::vector<T>& images, 
+                          size_t pixels,
+                          std::vector<float>& ret) {
+    size_t n = images.size() / pixels;
+    for (size_t i = 0; i < pixels; ++i) {
+        float total = 0.0f;
+        for (size_t j = 0; j < n; ++j) {
+            total += static_cast<float>(images[pixels * j + i]);
+        }
+        ret[i] = total / n;
+    }
+}
+
+inline void computeReciprocal(const std::vector<raw_dtype>& darks,
+                              const std::vector<raw_dtype>& flats,
+                              size_t pixels,
+                              std::vector<float>& reciprocal,
+                              std::vector<float>& dark_avg) {
+    spdlog::info("Computing reciprocal for flat fielding ...");
+
+#if defined(WITH_MONITOR)
+    auto start = std::chrono::steady_clock::now();
+#endif
+
+    averageImages<raw_dtype>(darks, pixels, dark_avg);
+    std::vector<float> flat_avg(dark_avg.size());
+    averageImages<raw_dtype>(flats, pixels, flat_avg);
+    for (size_t i = 0; i < pixels; ++i) {
+        if (dark_avg[i] == flat_avg[i]) {
+            reciprocal[i] = 1.0f;
+        } else {
+            reciprocal[i] = 1.0f / (flat_avg[i] - dark_avg[i]);
+        }
+    }
+
+#if defined(WITH_MONITOR)
+    float duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() -  start).count();
+    spdlog::info("**************************************************************");
+    spdlog::info("Computing reciprocal took {} ms", duration/1000);
+    spdlog::info("**************************************************************");
+#endif
+}
 
 } // slicerecon::utils
--- slicerecon/tests/test_reconstructor.cpp
+++ slicerecon/tests/test_reconstructor.cpp
@@ -1,9 +1,18 @@
+#include <vector>
+
 #include <gtest/gtest.h>
-#include "gmock/gmock.h"
+#include <gmock/gmock.h>
 
 #include "slicerecon/slicerecon.hpp"
+#include "slicerecon/data_types.hpp"
+#include "slicerecon/reconstruction/utils.hpp"
+
 
 namespace slicerecon::test {
+
+using ::testing::ElementsAreArray;
+using ::testing::Pointwise;
+using ::testing::FloatNear;
 
 class ReconTest : public testing::Test {
   protected:
@@ -43,7 +52,7 @@ TEST_F(ReconTest, TestPushProjectionException) {
         ProjectionType::dark, 0, {10, 10}, reinterpret_cast<char*>(img.data())), 
         std::runtime_error);
 }
-
+ 
 TEST_F(ReconTest, TestPushProjection) {
     std::vector<raw_dtype> img(pixels_);
 
@@ -73,6 +82,35 @@ TEST_F(ReconTest, TestPushProjection) {
     }
     EXPECT_EQ(recon_.buffer()[0], 10.f);
     EXPECT_EQ(recon_.buffer()[(projections_ - 1) * pixels_ - 1], 10.f * (projections_ - 1)); 
+}
+
+TEST(TestUtils, TestComputeReciprocal) {
+    int n = 3, pixels = 12;
+    std::vector<raw_dtype> darks {
+        4, 1, 1, 2, 0, 9, 7, 4, 3, 8, 6, 8, 
+        1, 7, 3, 0, 6, 6, 0, 8, 1, 8, 4, 2, 
+        2, 4, 6, 0, 9, 5, 8, 3, 4, 2, 2, 0
+    };
+    std::vector<raw_dtype> flats {
+        1, 9, 5, 1, 7, 9, 0, 6, 7, 1, 5, 6, 
+        2, 4, 8, 1, 3, 9, 5, 6, 1, 1, 1, 7, 
+        9, 9, 4, 1, 6, 8, 6, 9, 2, 4, 9, 4
+    };
+    std::vector<float> reciprocal(pixels);
+    std::vector<float> dark_avg(pixels);
+
+    slicerecon::utils::computeReciprocal(darks, flats, pixels, reciprocal, dark_avg);
+
+    EXPECT_THAT(dark_avg, ElementsAreArray({
+        2.33333333, 4.,        3.33333333,
+        0.66666667, 5.,        6.66666667,
+        5.,         5.,        2.66666667,
+        6.,         4.,        3.33333333}));
+    EXPECT_THAT(reciprocal, Pointwise(FloatNear(1e-6), {
+        0.59999996,  0.29999998, 0.42857143,
+        3.0000002,   2.9999986,  0.5,
+        -0.75000006, 0.5,        1.5000004,
+        -0.25,       1.,         0.42857143}));
 }
 
 } // slicerecon::test
