45e88007bd4a3afdf4f11fb386b8a3fea076e821
Jun Zhu <zhujun981661@gmail.com>
2022-08-06 11:33:59 +0200

    [GUI] Run on MacOS

--- gui/src/graphics/renderer.cpp
+++ gui/src/graphics/renderer.cpp
@@ -22,6 +22,9 @@ Renderer::Renderer() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_SAMPLES, 4);
 
     window_ = glfwCreateWindow(1920, 1080, "TOMCAT 3D Live Reconstruction", NULL, NULL);
