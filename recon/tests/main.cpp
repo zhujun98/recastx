2ec219e1391eda8f2817432306fb2300f3c9d8da
Jun Zhu <zhujun981661@gmail.com>
2022-11-28 11:22:27 +0100

    Change the level of spdlog in the unittest

--- recon/tests/main.cpp
+++ recon/tests/main.cpp
@@ -1,8 +1,12 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include <spdlog/spdlog.h>
+
 
 int main(int argc, char **argv) {
+  spdlog::set_level(spdlog::level::warn);
+
   ::testing::InitGoogleTest(&argc, argv);
 
   return RUN_ALL_TESTS();
