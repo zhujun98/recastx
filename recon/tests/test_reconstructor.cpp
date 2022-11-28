17c989d52b0ecf5127cabf80b91c2f19877502e7
Jun Zhu <zhujun981661@gmail.com>
2022-11-28 11:34:35 +0100

    Move TestComputeReciprocol to test_utils.cpp

--- recon/tests/test_reconstructor.cpp
+++ recon/tests/test_reconstructor.cpp
@@ -11,7 +11,6 @@
 
 namespace tomcat::recon::test {
 
-using ::testing::ElementsAreArray;
 using ::testing::Pointwise;
 using ::testing::FloatNear;
 
@@ -176,35 +175,6 @@ TEST_F(ReconTest, TestPushProjectionUnordered) {
     pushProjection(group_size_ + overflow, 2 * group_size_ - 1);
     // trigger warn log message, there must be at least one unfilled group in the buffer
     pushProjection(0, 1);
-}
-
-TEST(TestUtils, TestComputeReciprocal) {
-    int pixels = 12;
-    std::vector<RawDtype> darks {
-        4, 1, 1, 2, 0, 9, 7, 4, 3, 8, 6, 8, 
-        1, 7, 3, 0, 6, 6, 0, 8, 1, 8, 4, 2, 
-        2, 4, 6, 0, 9, 5, 8, 3, 4, 2, 2, 0
-    };
-    std::vector<RawDtype> flats {
-        1, 9, 5, 1, 7, 9, 0, 6, 7, 1, 5, 6, 
-        2, 4, 8, 1, 3, 9, 5, 6, 1, 1, 1, 7, 
-        9, 9, 4, 1, 6, 8, 6, 9, 2, 4, 9, 4
-    };
-    std::vector<float> reciprocal(pixels);
-    std::vector<float> dark_avg(pixels);
-
-    utils::computeReciprocal(darks, flats, pixels, reciprocal, dark_avg);
-
-    EXPECT_THAT(dark_avg, ElementsAreArray({
-        2.33333333, 4.,        3.33333333,
-        0.66666667, 5.,        6.66666667,
-        5.,         5.,        2.66666667,
-        6.,         4.,        3.33333333}));
-    EXPECT_THAT(reciprocal, Pointwise(FloatNear(1e-6), {
-        0.59999996,  0.29999998, 0.42857143,
-        3.0000002,   2.9999986,  0.5,
-        -0.75000006, 0.5,        1.5000004,
-        -0.25,       1.,         0.42857143}));
 }
 
 } // tomcat::recon::test
