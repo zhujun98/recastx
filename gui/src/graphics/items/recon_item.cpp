dd22dfac77b1795a77dcd9c57204a0fcc6081f14
Jun Zhu <zhujun981661@gmail.com>
2023-07-01 12:26:17 +0200

    Add copyright statement to each file

--- gui/src/graphics/items/recon_item.cpp
+++ gui/src/graphics/items/recon_item.cpp
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) Paul Scherrer Institut PSI
+ * Author: Jun Zhu
+ *
+ * Algorithms for drag-and-drop slices in 3D space are originally from https://github.com/cicwi/RECAST3D.git.
+ *
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+*/
 #include <cassert>
 
 #include <glm/gtc/constants.hpp>
--- gui/src/graphics/scene3d.cpp
+++ gui/src/graphics/scene3d.cpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Paul Scherrer Institut PSI
+ * Author: Jun Zhu
+ *
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+*/
 #include <algorithm>
 
 #include <glm/glm.hpp>
