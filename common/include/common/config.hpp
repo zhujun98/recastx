c22965ada1cc205d2b956bc3d52f6f112ebb7ad7
Jun Zhu <zhujun981661@gmail.com>
2024-01-19 09:48:21 +0100

    Change license

--- common/include/common/config.hpp
+++ common/include/common/config.hpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Paul Scherrer Institut
+ * Author: Jun Zhu
+ *
+ * Distributed under the terms of the BSD 3-Clause License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+*/
 #ifndef COMMON_CONFIG_H
 #define COMMON_CONFIG_H
 
--- common/include/common/scoped_timer.hpp
+++ common/include/common/scoped_timer.hpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Paul Scherrer Institut
+ * Author: Jun Zhu
+ *
+ * Distributed under the terms of the BSD 3-Clause License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+*/
 #ifndef COMMON_SCOPEDTIMER_HPP
 #define COMMON_SCOPEDTIMER_HPP
 
