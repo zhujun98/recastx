1c1f00dfd7ede3f789495c1f56f7c434231844a9
Jun Zhu <zhujun981661@gmail.com>
2024-08-21 16:17:32 +0200

    Minor

--- gui/src/fps_counter.cpp
+++ gui/src/fps_counter.cpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Paul Scherrer Institut
+ * Author: Jun Zhu
+ *
+ * Distributed under the terms of the BSD 3-Clause License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+*/
 #include "fps_counter.hpp"
 
 namespace recastx::gui {
